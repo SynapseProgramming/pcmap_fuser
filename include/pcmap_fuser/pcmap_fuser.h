4aa7450a8d627a237d46be798b3357e697d89921
Roald Ong <roald.ong@gmail.com>
2024-03-14 15:01:18 +0800

    feat(pcmap_fuser): Add in tf publisher based off initial pose estimate

--- include/pcmap_fuser/pcmap_fuser.h
+++ include/pcmap_fuser/pcmap_fuser.h
@@ -1,11 +1,31 @@
 #ifndef PCMAP_FUSER_H
 #define PCMAP_FUSER_H
 
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <ros/ros.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
 
 class PCMapFuser {
  public:
   PCMapFuser(ros::NodeHandle &nh, ros::NodeHandle &pnh);
+
+ private:
+  void initPoseCallback(
+      const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg);
+
+  void tfTimerCallback(const ros::TimerEvent &event);
+
+  ros::NodeHandle m_nh;
+  ros::NodeHandle m_pnh;
+  ros::Subscriber m_init_pose_sub;
+
+  ros::Timer m_tf_timer;
+
+  tf2_ros::TransformBroadcaster m_broadcaster;
+
+  geometry_msgs::TransformStamped m_fixed_floating_transform;
 };
 
 #endif  // PCMAP_FUSER_H
--- src/pcmap_fuser.cpp
+++ src/pcmap_fuser.cpp
@@ -3,5 +3,34 @@
 #include <iostream>
 
 PCMapFuser::PCMapFuser(ros::NodeHandle &nh, ros::NodeHandle &pnh) {
-  std::cout << "test\n";
+  m_nh = nh;
+  m_pnh = pnh;
+  m_init_pose_sub =
+      m_nh.subscribe("/initialpose", 1, &PCMapFuser::initPoseCallback, this);
+
+  m_tf_timer =
+      m_nh.createTimer(ros::Duration(0.1), &PCMapFuser::tfTimerCallback, this);
+
+  // set initial values
+  m_fixed_floating_transform.transform.translation.x = 1;
+  m_fixed_floating_transform.transform.rotation.w = 1;
+  m_fixed_floating_transform.header.frame_id = "map";
+  m_fixed_floating_transform.child_frame_id = "base_link";
+}
+
+void PCMapFuser::initPoseCallback(
+    const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg) {
+  m_fixed_floating_transform.transform.translation.x =
+      msg->pose.pose.position.x;
+  m_fixed_floating_transform.transform.translation.y =
+      msg->pose.pose.position.y;
+  m_fixed_floating_transform.transform.translation.z =
+      msg->pose.pose.position.z;
+  m_fixed_floating_transform.transform.rotation = msg->pose.pose.orientation;
+}
+
+void PCMapFuser::tfTimerCallback(const ros::TimerEvent &event) {
+  std::cout << "Timer callback\n" << std::endl;
+  m_fixed_floating_transform.header.stamp = ros::Time::now();
+  m_broadcaster.sendTransform(m_fixed_floating_transform);
 }
